fbe1030977c6f6a85e481e7f06fa761d786bba6b
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-25 19:24:38 -0300

    Add function of country request

--- providers/nowsh/service.go
+++ providers/nowsh/service.go
@@ -8,12 +8,26 @@ import (
 
 type service interface {
 	RequestCountries() (io.Reader, error)
+	RequestCountry(countryName string) (io.Reader, error)
 }
 
 type requestService struct{}
 
 func (s *requestService) RequestCountries() (io.Reader, error) {
-	resp, err := http.Get(NowShURL)
+	resp, err := http.Get(NowShURLAll)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("status != 200")
+	}
+
+	return resp.Body, nil
+}
+
+func (s *requestService) RequestCountry(countryName string) (io.Reader, error) {
+	resp, err := http.Get(NowShURLOn + countryName)
 	if err != nil {
 		return nil, err
 	}
--- providers/nowsh/service_test.go
+++ providers/nowsh/service_test.go
@@ -10,3 +10,12 @@ func TestRequestServiceRequestCountries(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestRequestServiceRequestCountry(t *testing.T) {
+	service := &requestService{}
+
+	_, err := service.RequestCountry("Brazil")
+	if err != nil {
+		t.Fail()
+	}
+}
