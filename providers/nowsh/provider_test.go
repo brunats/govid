c21a00ff4531dc24f1bcf90e06f7d9716dffde23
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-25 15:40:50 -0300

    Add tests to RequestCountries with error

--- providers/nowsh/provider_test.go
+++ providers/nowsh/provider_test.go
@@ -2,11 +2,32 @@ package nowsh
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"sync"
 	"testing"
 )
+
+type serviceFake interface {
+	RequestCountries() (io.Reader, error)
+}
+
+type requestServiceFakeWithoutErrors struct{}
+type requestServiceFakeWithErrors struct{}
+
+func (s *requestServiceFakeWithoutErrors) RequestCountries() (io.Reader, error) {
+	file, _ := os.Open("allCountries.json")
+	reader := bufio.NewReader(file)
+
+	return reader, nil
+}
+
+func (s *requestServiceFakeWithErrors) RequestCountries() (io.Reader, error) {
+	err := fmt.Errorf("fake error")
+
+	return nil, err
+}
 
 func TestNew(t *testing.T) {
 	provider := New()
@@ -16,29 +37,29 @@ func TestNew(t *testing.T) {
 	}
 }
 
-type serviceFake interface {
-	RequestCountries() (io.Reader, error)
-}
-
-type requestServiceFake struct{}
-
-func (s *requestServiceFake) RequestCountries() (io.Reader, error) {
-	file, _ := os.Open("allCountries.json")
-	reader := bufio.NewReader(file)
-
-	return reader, nil
-}
-
 func TestRequestCountries(t *testing.T) {
 	wg := sync.WaitGroup{}
 
-	provider := &provider{}
-	provider.wg = wg
-	provider.service = &requestServiceFake{}
-
+	provider := &provider{wg: wg, service: &requestServiceFakeWithoutErrors{}}
 	provider.requestCountries()
 
 	if len(provider.response) != 5 {
+		t.Fail()
+	}
+}
+
+func TestRequestCountriesWithError(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	provider := &provider{wg: wg, service: &requestServiceFakeWithErrors{}}
+
+	provider.requestCountries()
+
+	if len(provider.response) > 1 {
+		t.Fail()
+	}
+
+	if provider.response[0].Error == nil {
 		t.Fail()
 	}
 }
