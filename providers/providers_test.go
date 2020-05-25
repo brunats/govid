b7ac92fe151516bed3f3ba93fa72e5092c2580df
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2020-05-24 22:08:56 -0300

    Add providers inicialization

---
+++ providers/providers_test.go
@@ -0,0 +1,25 @@
+package providers
+
+import (
+	"context"
+	"testing"
+)
+
+type ProviderFake struct {
+	ResponseMock []Data
+}
+
+func (p *ProviderFake) Request(ctx context.Context) {}
+func (p *ProviderFake) Wait()    {}
+
+func (p *ProviderFake) Response() []Data {
+	return p.ResponseMock
+}
+
+func TestRegisterProviders(t *testing.T) {
+	Register(&ProviderFake{})
+
+	if len(Providers()) != 1 {
+		t.Fail()
+	}
+}
